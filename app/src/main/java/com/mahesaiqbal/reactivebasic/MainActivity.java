e72ada71d64f4cf3cec7767cd877fe97c3547091
iqbal <mahesa@popbox.asia>
2018-12-16 19:59:37 +0700

    First commit

---
+++ app/src/main/java/com/mahesaiqbal/reactivebasic/MainActivity.java
@@ -0,0 +1,121 @@
+package com.mahesaiqbal.reactivebasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Observable<String> animalsObservable = getAnimalsObservable();
+
+        DisposableObserver<String> animalsObserver = getAnimalObserver();
+
+        DisposableObserver<String> animalsObserverAllCaps = getAnimalsAllCapsObserver();
+
+        compositeDisposable.add(
+                animalsObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .filter(new Predicate<String>() {
+                            @Override
+                            public boolean test(String s) throws Exception {
+                                return s.toLowerCase().startsWith("b");
+                            }
+                        })
+                        .subscribeWith(animalsObserver)
+        );
+
+        compositeDisposable.add(
+                animalsObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .filter(new Predicate<String>() {
+                            @Override
+                            public boolean test(String s) throws Exception {
+                                return s.toLowerCase().startsWith("c");
+                            }
+                        })
+                        .map(new Function<String, String>() {
+                            @Override
+                            public String apply(String s) {
+                                return s.toUpperCase();
+                            }
+                        })
+                        .subscribeWith(animalsObserverAllCaps)
+        );
+    }
+
+    private DisposableObserver<String> getAnimalObserver() {
+        return new DisposableObserver<String>() {
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "Name: " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "All items are emitted!");
+            }
+        };
+    }
+
+    private DisposableObserver<String> getAnimalsAllCapsObserver() {
+        return new DisposableObserver<String>() {
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "Name: " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "All items are emitted!");
+            }
+        };
+    }
+
+    private Observable<String> getAnimalsObservable() {
+        return Observable.fromArray(
+                "Ant", "Ape",
+                "Bat", "Bee", "Bear", "Butterfly",
+                "Cat", "Crab", "Cod",
+                "Dog", "Dove",
+                "Fox", "Frog"
+        );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        //Don't send events once the activity is destroyed
+        compositeDisposable.clear();
+        Log.d(TAG, "All data is discarded!");
+    }
+}
