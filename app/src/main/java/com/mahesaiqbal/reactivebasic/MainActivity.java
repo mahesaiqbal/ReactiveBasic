88a543d82cf3f655c6c75c23aae9373b4a5cfbe1
iqbal <mahesa@popbox.asia>
2018-12-18 11:08:33 +0700

    Learn operators on RxJava

--- app/src/main/java/com/mahesaiqbal/reactivebasic/MainActivity.java
+++ app/src/main/java/com/mahesaiqbal/reactivebasic/MainActivity.java
@@ -12,9 +12,12 @@ import java.util.List;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
 import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
@@ -22,85 +25,54 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private CompositeDisposable compositeDisposable = new CompositeDisposable();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // add to Composite observable
-        // .map() operator is used to turn the note into all uppercase letters
-        compositeDisposable.add(
-                getNotesObservable()
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .map(new Function<Note, Note>() {
-                            @Override
-                            public Note apply(Note note) throws Exception {
-                                // Making the note to all uppercase
-                                note.setNote(note.getNote().toUpperCase());
-                                return note;
-                            }
-                        })
-                        .subscribeWith(getNotesObserver())
-        );
-    }
+//        Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+//                11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
-    private DisposableObserver<Note> getNotesObserver() {
-        return new DisposableObserver<Note>() {
-
-            @Override
-            public void onNext(Note note) {
-                Log.d(TAG, "Note: " + note.getNote());
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.e(TAG, "onError: " + e.getMessage());
-            }
-
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "All notes are emitted!");
-            }
-        };
-    }
-
-    private Observable<Note> getNotesObservable() {
-        final List<Note> notes = prepareNotes();
-
-        return Observable.create(new ObservableOnSubscribe<Note>() {
-            @Override
-            public void subscribe(ObservableEmitter<Note> emitter) throws Exception {
-                for (Note note: notes) {
-                    if (!emitter.isDisposed()) {
-                        emitter.onNext(note);
+        Observable.range(1, 20)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .filter(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer integer) throws Exception {
+                        return integer % 2 == 0;
                     }
-                }
+                })
+                .map(new Function<Integer, String>() {
+                    @Override
+                    public String apply(Integer integer) throws Exception {
+                        return integer + " is even number";
+                    }
+                })
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
 
-                if (!emitter.isDisposed()) {
-                    emitter.onComplete();
-                }
-            }
-        });
-    }
+                    }
 
-    private List<Note> prepareNotes() {
-        List<Note> notes = new ArrayList<>();
-        notes.add(new Note(1, "Belajar konsep dasar RxJava"));
-        notes.add(new Note(2, "Belajar operator dasar RxJava"));
-        notes.add(new Note(3, "Belajar tipe interface dasar RxJava"));
-        notes.add(new Note(4, "Mulai membuat aplikasi Android dengan RxJava"));
+                    @Override
+                    public void onNext(String s) {
+                        Log.d(TAG, "onNext: " + s);
+                    }
 
-        return notes;
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "All numbers emitted!");
+                    }
+                });
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-        compositeDisposable.clear();
-        Log.d(TAG, "All notes is cleared!");
     }
 }
