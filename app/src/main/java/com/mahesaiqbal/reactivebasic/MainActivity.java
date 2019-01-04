73ec190ff0c77b6b1615227230b2d02214cc6d44
iqbal <mahesa@popbox.asia>
2019-01-04 15:03:03 +0700

    SwitchMap() operator

--- app/src/main/java/com/mahesaiqbal/reactivebasic/MainActivity.java
+++ app/src/main/java/com/mahesaiqbal/reactivebasic/MainActivity.java
@@ -4,16 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.mahesaiqbal.reactivebasic.model.Address;
-import com.mahesaiqbal.reactivebasic.model.User;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
@@ -30,104 +24,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getUsersObservable()
+        Observable<Integer> integerObservable = Observable.fromArray(new Integer[]{1, 2, 3, 4, 5, 6});
+
+        // it always emits 6 as it un-subscribes the before observer
+        integerObservable
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .concatMap(new Function<User, Observable<User>>() {
+                .switchMap(new Function<Integer, ObservableSource<Integer>>() {
                     @Override
-                    public Observable<User> apply(User user) throws Exception {
-                        // getting each user address by making another network call
-
-                        return getAddressObservable(user);
+                    public ObservableSource<Integer> apply(Integer integer) throws Exception {
+                        return Observable.just(integer).delay(1, TimeUnit.SECONDS);
                     }
-                }).subscribe(new Observer<User>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe");
-                disposable = d;
-            }
-
-            @Override
-            public void onNext(User user) {
-                Log.d(TAG, "onNext: " + user.getName() + ", " + user.getGender() + ", " + user.getAddress().getAddress());
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.e(TAG, e.getMessage());
-            }
-
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "All users emitted!");
-            }
-        });
-    }
-
-    /**
-     * Assume this as a network call
-     * returns Users with address filed added
-     */
-    private Observable<User> getAddressObservable(final User user) {
-        final String[] addresses = new String[]{
-                "1600 Amphitheatre Parkway, Mountain View, CA 94043",
-                "2300 Traverwood Dr. Ann Arbor, MI 48105",
-                "500 W 2nd St Suite 2900 Austin, TX 78701",
-                "355 Main Street Cambridge, MA 02142"
-        };
-
-        return Observable.create(new ObservableOnSubscribe<User>() {
-            @Override
-            public void subscribe(ObservableEmitter<User> emitter) throws Exception {
-                Address address = new Address();
-                address.setAddress(addresses[new Random().nextInt(2) + 0]);
-
-                if (!emitter.isDisposed()) {
-                    user.setAddress(address);
-
-                    // Generate network latency of random duration
-                    int sleepTime = new Random().nextInt(1000) + 500;
-
-                    Thread.sleep(sleepTime);
-                    emitter.onNext(user);
-                    emitter.onComplete();
-                }
-            }
-        }).subscribeOn(Schedulers.io());
-    }
-
-    /**
-     * Assume this is a network call to fetch users
-     * returns Users with name and gender but missing address
-     */
-    private Observable<User> getUsersObservable() {
-        String[] maleUsers = new String[]{"Mark", "John", "Trump", "Obama"};
-
-        final List<User> users = new ArrayList<>();
-
-        for (String name : maleUsers) {
-            User user = new User();
-            user.setName(name);
-            user.setGender("male");
-
-            users.add(user);
-        }
-
-        return Observable
-                .create(new ObservableOnSubscribe<User>() {
+                })
+                .subscribe(new Observer<Integer>() {
                     @Override
-                    public void subscribe(ObservableEmitter<User> emitter) throws Exception {
-                        for (User user : users) {
-                            if (!emitter.isDisposed()) {
-                                emitter.onNext(user);
-                            }
-                        }
-
-                        if (!emitter.isDisposed()) {
-                            emitter.onComplete();
-                        }
+                    public void onSubscribe(Disposable d) {
+                        Log.d(TAG, "onSubscribe");
+                        disposable = d;
                     }
-                }).subscribeOn(Schedulers.io());
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.d(TAG, "onNext: " + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "onError: " + e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "All users emitted!");
+                    }
+                });
     }
 
     @Override
