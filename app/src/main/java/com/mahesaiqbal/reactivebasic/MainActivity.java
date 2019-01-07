16f8b956ed268d032df9648dfaa2ec8bbb76213a
iqbal <mahesa@popbox.asia>
2019-01-07 15:11:22 +0700

    Debounce operator

--- app/src/main/java/com/mahesaiqbal/reactivebasic/MainActivity.java
+++ app/src/main/java/com/mahesaiqbal/reactivebasic/MainActivity.java
@@ -4,9 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.jakewharton.rxbinding2.view.RxView;
+import com.jakewharton.rxbinding2.widget.RxTextView;
+import com.jakewharton.rxbinding2.widget.TextViewTextChangeEvent;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -16,25 +19,24 @@ import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Function;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    @BindView(R.id.tap_result)
-    TextView txtTapResult;
+    @BindView(R.id.input_search)
+    EditText inputSearch;
 
-    @BindView(R.id.tap_result_max_count)
-    TextView txtTapResultMax;
+    @BindView(R.id.txt_search_string)
+    TextView txtSearchString;
 
-    @BindView(R.id.layout_tap_area)
-    Button btnTapArea;
-
-    private Disposable disposable;
+    private CompositeDisposable disposable = new CompositeDisposable();
     private Unbinder unbinder;
-    private int maxTaps = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,47 +44,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         unbinder = ButterKnife.bind(this);
 
-        RxView.clicks(btnTapArea)
-                .map(new Function<Object, Integer>() {
-                    @Override
-                    public Integer apply(Object o) throws Exception {
-                        return 1;
-                    }
-                })
-                .buffer(3, TimeUnit.SECONDS)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(new Observer<List<Integer>>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-                        disposable = d;
-                    }
+        disposable.add(
+                RxTextView.textChangeEvents(inputSearch)
+                        .skipInitialValue()
+                        .debounce(300, TimeUnit.MILLISECONDS)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(searchQuery()));
 
-                    @Override
-                    public void onNext(List<Integer> integers) {
-                        Log.d(TAG, "onNext: " + integers.size() + " taps received!");
-                        if (integers.size() > 0) {
-                            maxTaps = integers.size() > maxTaps ? integers.size() : maxTaps;
-                            txtTapResult.setText(String.format("Received %d taps in 3 secs", integers.size()));
-                            txtTapResultMax.setText(String.format("Maximum of %d taps received in this session", maxTaps));
-                        }
-                    }
+        txtSearchString.setText("Search query will be accumulated every 300 milli sec");
+    }
 
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.e(TAG, "onError: " + e.getMessage());
-                    }
+    private DisposableObserver<TextViewTextChangeEvent> searchQuery() {
+        return new DisposableObserver<TextViewTextChangeEvent>() {
+            @Override
+            public void onNext(TextViewTextChangeEvent textViewTextChangeEvent) {
+                Log.d(TAG, "search string: " + textViewTextChangeEvent.text().toString());
+                txtSearchString.setText(textViewTextChangeEvent.text().toString());
+            }
 
-                    @Override
-                    public void onComplete() {
-                        Log.e(TAG, "onComplete");
-                    }
-                });
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete");
+            }
+        };
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         unbinder.unbind();
-        disposable.dispose();
+        disposable.clear();
     }
 }
